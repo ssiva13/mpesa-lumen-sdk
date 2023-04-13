444a04f510f0fcee340fee0b075efe25438eb02d
Simon Siva <simonsiva13@gmail.com>
2023-04-13 21:25:50 +0300

    register service provider

--- src/MpesaServiceProvider.php
+++ src/MpesaServiceProvider.php
@@ -9,8 +9,27 @@
 namespace Ssiva\MpesaLumenSdk;
 
 use Illuminate\Support\ServiceProvider;
+use Ssiva\MpesaDaraja\Mpesa;
 
 class MpesaServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        // $this->publishes([
+        //     __DIR__.'/../config/mpesa.php' => config_path('mpesa.php')
+        // ], 'mpesa_config');
+    }
     
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->bind('mpesa-daraja', function () {
+            return new Mpesa();
+        });
+    }
 }
